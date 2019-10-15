b72710f3d1539874efa6b1386bca754dd46dae6b
darrikpuetz <darrikpuetz@gmail.com>
2019-10-15 15:48:11 -0700

    exceptions handled

--- NumbersGame/NumbersGame/Program.cs
+++ NumbersGame/NumbersGame/Program.cs
@@ -9,7 +9,7 @@ namespace NumbersGame
             Console.WriteLine("Enter Something");
             try
             {
-            StartSequence();
+                StartSequence();
 
             }
             catch (Exception e)
@@ -21,22 +21,42 @@ namespace NumbersGame
                 Console.WriteLine("Program is done");
             }
         }
-
+        /// <summary>
+        /// Runs the Populate, GetSum, GetProduct, and GetQuotient methods and writes to the console how they were created. 
+        /// </summary>
         public static void StartSequence()
         {
+            try
+            {
             Console.WriteLine("Enter a number greater than zero");
-            int input = Convert.ToInt32(Console.ReadLine());
+            string inputString = Console.ReadLine();
+            int input = Convert.ToInt32(inputString);
             int[] startarr = new int[input];
-            int [] pop = Populate(startarr);
+            int[] pop = Populate(startarr);
             int sum = GetSum(pop);
             int product = GetProduct(pop, sum);
             decimal quo = GetQuotient(product);
 
             Console.WriteLine($"Your array size is: {pop.Length} ");
-            Console.WriteLine($"The numbers in the array are {String.Join(", " , pop) }");
-            Console.WriteLine(" * = ");
-            Console.WriteLine(" /  = ");
+            Console.WriteLine($"The numbers in the array are {String.Join(", ", pop) }");
+            Console.WriteLine($"{sum} * {product / sum} = {product} ");
+            Console.WriteLine($"{product} / {product / quo}  = {quo} ");
 
+
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception )
+            {
+
+                throw;
+            }
         }
         /// <summary>
         /// The Populate method is taking an input from the number entered and returning an array for the request number that is a long as the number entered.
@@ -48,15 +68,16 @@ namespace NumbersGame
             for (int i = 0; i < arr.Length; i++)
             {
                 Console.WriteLine($"Please enter a number {i + 1}/{arr.Length}");
-                arr[i] = Convert.ToInt32(Console.ReadLine());
+                string input = Console.ReadLine();
+                arr[i] = Convert.ToInt32(input);
             }
             return arr;
         }
         /// <summary>
-        /// 
+        /// Iterates through the an array and adds to the sum the different values that were inputed.
         /// </summary>
         /// <param name="sumArr"></param>
-        /// <returns></returns>
+        /// <returns>returns the variable sum</returns>
         public static int GetSum(int[] sumArr)
         {
             int sum = 0;
@@ -74,19 +95,20 @@ namespace NumbersGame
 
         }
         /// <summary>
-        /// 
+        /// Multiples the specific index of the array to the number the user entered.
         /// </summary>
         /// <param name="productArr"></param>
         /// <param name="productSum"></param>
-        /// <returns></returns>
+        /// <returns>returns the product of the two numbers</returns>
         public static int GetProduct(int[] productArr, int productSum)
         {
             try
             {
-            Console.WriteLine($"Select a random number between 1 and {productArr.Length} ");
-            int input = Convert.ToInt32(Console.ReadLine());
-            int product = productSum * productArr[input - 1];
-            return product;
+                Console.WriteLine($"Select a random number between 1 and {productArr.Length} ");
+                string inputString = Console.ReadLine();
+                int input = Convert.ToInt32(inputString);
+                int product = productSum * productArr[input - 1];
+                return product;
 
             }
             catch (Exception e)
@@ -96,17 +118,18 @@ namespace NumbersGame
             }
         }
         /// <summary>
-        /// 
+        /// Uses the input of the user to divide the number that was chosen in the prior method.
         /// </summary>
         /// <param name="product"></param>
-        /// <returns></returns>
+        /// <returns>returns 0 if the the catch is enabled</returns>
         public static decimal GetQuotient(int product)
         {
             try
             {
                 Console.WriteLine("Enter a number to divide by. ");
-                int input = Convert.ToInt32(Console.ReadLine());
-                decimal quotient = Decimal.Divide(product,input);
+                string inputString = Console.ReadLine();
+                int input = Convert.ToInt32(inputString);
+                decimal quotient = Decimal.Divide(product, input);
 
                 return quotient;
 
